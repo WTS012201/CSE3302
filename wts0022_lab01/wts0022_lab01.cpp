e7ce6e1fdb779b49bde92d40674c84b46c7b979d
Will <WTS012201@gmail.com>
2022-02-09 21:43:27 -0600

    questions

--- wts0022_lab01/wts0022_lab01.cpp
+++ wts0022_lab01/wts0022_lab01.cpp
@@ -1,3 +1,23 @@
+/*
+1)  Was one language easier or faster to write the code for this?  If so, describe in 
+detail why, as in what about the language made that the case. 
+    Python was the easiest language to write this in. Didn't have to deal with semicolons,
+    types, brackets or blocks, and namespaces.
+
+2)  Even though a language may not (e.g. FORTRAN) does not support recursion, 
+describe how you could write a program to produce the same results without 
+using recursion.  Would that approach have any limitations and if so, what would 
+they be?  
+    Instead of using recursion, you could just do it iterativly but it would require more work.
+    Perhaps you could run a while loop that gets the size of all the files. 
+    If it finds a subfolder, put it in a stack or queue. Open folder in stack
+    or queue once there are no files left and repeat the process until they are empty.
+
+    There are no apparent limitations with this approach. As far as resource use goes,
+    the stack or queue would be negligible, but it would require more memory than the
+    recursive solution.
+*/
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -6,16 +26,16 @@
 int get_size(std::string path){
     auto total = 0;
     
-    for(const auto& elem : std::filesystem::directory_iterator(path)){
+    for(const auto& elem : std::filesystem::directory_iterator(path))
         if(std::filesystem::is_directory(elem.path()))
             total += get_size(elem.path());
         else
             total += std::filesystem::file_size(elem.path());
-    }
     return total;
 }
 
 int main(){
     auto total_size = get_size(std::filesystem::current_path());
+
     std::cout << total_size << " bytes" << std::endl;
 }
